685eedcbf647207383dbbcf10559cedab2051d1e
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-03-26 17:32:24 -0700

    added a click box that accepts new values and modifies the name on the name tag

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+// testing to see that the pages have attached correctly
+
 console.log('hello world');
 
-const name = document.getElementById ('name');
+//to change the name of the name tag from the HTML
+
+const name = document.getElementById('name');
+
+const enterName = document.getElementById('enterName');
 
 name.style.color = 'purple';
 
 name.textContent = 'Cheese Eater';
+
+function logOut() {
+    console.log('Change the name!', Date.now());
+    const enterName = document.getElementById('enterName');
+    const currentInput = enterName.value;
+    name.textContent = currentInput;
+}
+
+//to change the name of the name tag by the user
+
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', logOut);
